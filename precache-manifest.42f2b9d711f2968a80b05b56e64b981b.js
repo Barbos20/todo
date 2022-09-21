3ac409bb33b0bbb11c2a549492ab920de95eefe5
Barbos20 <artur.korol.2000.10.25@mail.ru>
2022-09-21 12:53:41 +0300

    Updates

---
+++ precache-manifest.42f2b9d711f2968a80b05b56e64b981b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3b857047e3c3ea8d2f627d56521c03",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "f1e0e6a4dc275c72ec78",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "ebb958f156e87348839f",
+    "url": "/todolist/static/js/2.c03ffd9b.chunk.js"
+  },
+  {
+    "revision": "5373203bd4fde313a2106bcb2e6c7cd3",
+    "url": "/todolist/static/js/2.c03ffd9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1e0e6a4dc275c72ec78",
+    "url": "/todolist/static/js/main.49a2dc71.chunk.js"
+  },
+  {
+    "revision": "f6ebc9bad2a2c9c79d0c",
+    "url": "/todolist/static/js/runtime-main.39f6e27a.js"
+  }
+]);
